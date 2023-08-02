dfa55647392c006cf94deb49e13c3e961c96e9a2
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-08-02 13:25:35 -0400

    ecco_mode_water

--- ecco_mode_water.jl
+++ ecco_mode_water.jl
@@ -1,8 +1,11 @@
 using JLD2
 using Oceananigans
 using Oceananigans.Utils
+using Oceananigans.Grids: architecture, node
 using KernelAbstractions: @kernel, @index
+using Oceananigans.AbstractOperations: GridMetricOperation
 using NetCDF
+using CUDA
 using CairoMakie
 
 MetricField(loc, grid, metric) = compute!(Field(GridMetricOperation(loc, metric, grid)))
@@ -18,13 +21,12 @@ VolumeField(grid, loc = (Center, Center, Center)) = MetricField(loc, grid, Ocean
 end
 
 function calc_mode_water_volume(T, grid, volumes)
-    flag = CenterField(grid)
-    launch!(architecture(grid), grid, _flag_volumes, flag, T, grid)
-
-    return sum(flag * volumes)
+    flag = zeros(size(T)...)
+    launch!(architecture(grid), grid, :xyz, _flag_volumes, flag, T, grid)
+    return sum(flag .* volumes)
 end
 
-function load_and_setup_data(file, grid)
+function load_and_setup_data(file)
     data = ncread(file, "THETA")[:, :, :, 1]
     data[data .< -1e2] .= 0
     data = reverse(data, dims = 3)
@@ -32,7 +34,7 @@ function load_and_setup_data(file, grid)
     tmp  = zeros(nx ÷2, ny, nz)
     transpose_flux!(data, tmp)
 
-    return set!(CenterField(grid), data)
+    return data
 end
 
 function transpose_flux!(var, tmp)
@@ -40,10 +42,13 @@ function transpose_flux!(var, tmp)
     tmp .= var[nx+1:end, :, :]
     var[nx+1:end, :, :] .= var[1:nx, :, :]
     var[1:nx, :, :]     .= tmp
+
+    return nothing
 end
 
-const λW, λE = ( 26,   43)
-const φS, φN = (-81.5, -5.6)
+const λW, λE = (-81.5, -40)
+const φS, φN = ( 26.0, 43.0)
+const year   = 1995
 
 z_faces = ECCO_z_faces()
 ecco_grid = LatitudeLongitudeGrid(size = (1440, 720, 50),
@@ -51,12 +56,28 @@ ecco_grid = LatitudeLongitudeGrid(size = (1440, 720, 50),
                                  latitude = (-90, 90),
                                  z = z_faces)
 
-volumes = VolumeField(ecco_grid)
+volumes = Array(interior(VolumeField(ecco_grid)))
 
-V_all = []
+monthly_days() = [1:31, 1:28, 1:31, 1:30, 1:31, 1:30, 1:31, 1:31, 1:30, 1:31, 1:30, 1:31]
 
-for file in all_files
-    T = load_and_setup_data(file, ecco_grid)
-    V = calc_mode_water(T, ecco_grid, volumes)
-    push!(V_all, V)
+iters = []
+files = []
+
+for month in 1:12
+    for day in monthly_days()[month]
+        push!(iters, string(year) * string(month, pad=2) * string(day, pad=2))
+        push!(files, "THETA.1440x720x50." * iters[end] * ".nc")
+    end
 end
+
+mode_water = []
+days       = [] 
+
+for (file, day) in zip(files, days)
+    T = load_and_setup_data(file)
+    V = calc_mode_water(T, ecco_grid, volumes)
+    push!(mode_water, V)
+    push!(days, day)
+end
+
+jldsave("ecco_mode_water.jl", days = days, mode_water = mode_water)
