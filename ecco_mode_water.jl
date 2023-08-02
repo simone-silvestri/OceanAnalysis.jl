db4a752a8c55a15ac3adc0f3fc2fe09bb7378fb2
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-08-02 13:29:03 -0400

    ecco mode water

--- ecco_mode_water.jl
+++ ecco_mode_water.jl
@@ -1,62 +1,20 @@
-using JLD2
-using Oceananigans
-using Oceananigans.Utils
-using Oceananigans.Grids: architecture, node
-using KernelAbstractions: @kernel, @index
-using Oceananigans.AbstractOperations: GridMetricOperation
-using NetCDF
-using CUDA
-using CairoMakie
+using DataDeps
 
-MetricField(loc, grid, metric) = compute!(Field(GridMetricOperation(loc, metric, grid)))
-VolumeField(grid, loc = (Center, Center, Center)) = MetricField(loc, grid, Oceananigans.AbstractOperations.volume)
-
-@kernel function _flag_volumes(flag, T, grid)
-    i, j, k = @index(Global, NTuple)
-    λ, φ, _ = node(i, j, k, grid, Center(), Center(), Center())
-
-    if (λW < λ < λE) && (φS < φ < φN) && (17 < T[i, j, k] < 19)
-        flag[i, j, k] = 1
-    end
-end
-
-function calc_mode_water_volume(T, grid, volumes)
-    flag = zeros(size(T)...)
-    launch!(architecture(grid), grid, :xyz, _flag_volumes, flag, T, grid)
-    return sum(flag .* volumes)
-end
-
-function load_and_setup_data(file)
-    data = ncread(file, "THETA")[:, :, :, 1]
-    data[data .< -1e2] .= 0
-    data = reverse(data, dims = 3)
-    nx, ny, nz = size(data)
-    tmp  = zeros(nx ÷2, ny, nz)
-    transpose_flux!(data, tmp)
-
-    return data
-end
-
-function transpose_flux!(var, tmp)
-    nx = size(tmp, 1)
-    tmp .= var[nx+1:end, :, :]
-    var[nx+1:end, :, :] .= var[1:nx, :, :]
-    var[1:nx, :, :]     .= tmp
-
-    return nothing
-end
+include("ecco_z_faces.jl")
 
 const λW, λE = (-81.5, -40)
 const φS, φN = ( 26.0, 43.0)
 const year   = 1995
 
+arch = GPU()
+
 z_faces = ECCO_z_faces()
-ecco_grid = LatitudeLongitudeGrid(size = (1440, 720, 50),
+ecco_grid = LatitudeLongitudeGrid(arch; size = (1440, 720, 50),
                                  longitude = (-180, 180),
                                  latitude = (-90, 90),
                                  z = z_faces)
 
-volumes = Array(interior(VolumeField(ecco_grid)))
+volumes = arch_array(arch, interior(VolumeField(ecco_grid)))
 
 monthly_days() = [1:31, 1:28, 1:31, 1:30, 1:31, 1:30, 1:31, 1:31, 1:30, 1:31, 1:30, 1:31]
 
@@ -74,7 +32,7 @@ mode_water = []
 days       = [] 
 
 for (file, day) in zip(files, days)
-    T = load_and_setup_data(file)
+    T = load_and_setup_data(file, ecco_grid)
     V = calc_mode_water(T, ecco_grid, volumes)
     push!(mode_water, V)
     push!(days, day)
---
+++ utils.jl
@@ -0,0 +1,47 @@
+using JLD2
+using Oceananigans
+using Oceananigans.Utils
+using Oceananigans.Architectures: arch_array
+using Oceananigans.Grids: architecture, node
+using KernelAbstractions: @kernel, @index
+using Oceananigans.AbstractOperations: GridMetricOperation
+using NetCDF
+using CUDA
+
+MetricField(loc, grid, metric) = compute!(Field(GridMetricOperation(loc, metric, grid)))
+VolumeField(grid, loc = (Center, Center, Center)) = MetricField(loc, grid, Oceananigans.AbstractOperations.volume)
+
+@kernel function _flag_volumes(flag, T, grid)
+    i, j, k = @index(Global, NTuple)
+    λ, φ, _ = node(i, j, k, grid, Center(), Center(), Center())
+
+    if (λW < λ < λE) && (φS < φ < φN) && (17 < T[i, j, k] < 19)
+        flag[i, j, k] = 1
+    end
+end
+
+function calc_mode_water_volume(T, grid, volumes)
+    flag = arch_array(architecture(grid), zeros(size(T)...))
+    launch!(architecture(grid), grid, :xyz, _flag_volumes, flag, T, grid)
+    return sum(flag .* volumes)
+end
+
+function load_and_setup_data(file, grid)
+    data = ncread(file, "THETA")[:, :, :, 1]
+    data[data .< -1e2] .= 0
+    data = reverse(data, dims = 3)
+    nx, ny, nz = size(data)
+    tmp  = zeros(nx ÷2, ny, nz)
+    transpose_flux!(data, tmp)
+
+    return arch_array(architecture(grid), data)
+end
+
+function transpose_flux!(var, tmp)
+    nx = size(tmp, 1)
+    tmp .= var[nx+1:end, :, :]
+    var[nx+1:end, :, :] .= var[1:nx, :, :]
+    var[1:nx, :, :]     .= tmp
+
+    return nothing
+end
