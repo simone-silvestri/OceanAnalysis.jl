ebb85347cc4bcca7a9e9971ac3c726068ebe2d4a
Simone Silvestri <33547697+simone-silvestri@users.noreply.github.com>
2023-08-07 19:30:03 -0400

    see

--- utils.jl
+++ utils.jl
@@ -34,7 +34,7 @@ end
         ρ★ = ρ[i, j, k]
         k  = k-1
         h[i, j] = zero(grid)
-        @show ρ[i, j, k],  ρ★
+        @show abs(ρ[i, j, k] - ρ★) t
         while abs(ρ[i, j, k] - ρ★) < t && k > 1
             h[i, j] = znode(i, j, k, grid, Center(), Center(), Center())
             k -= 1
